7339848a1bc11fe601c0493570a4b26aa296df27
joejackson <joe.m.jackson@live.co.uk>
2020-02-14 18:01:52 +0000

    Define button values in HTML tags

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// 1. Define variable that will take arguements
+
+let equation = []
+
+// 2. Display current number on display
+
+// 3. Display number in calculation summary window in display
+
+
+// Flatten out array to do calculation (use a FOR loop to cocatenate everything?)
