fef7fed7b76999baedb63d6ac900d1c70ad559f3
joejackson <joe.m.jackson@live.co.uk>
2020-02-15 16:12:44 +0000

    Define function to grab button value

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
-// 1. Define variable that will take arguements
+// Define variable that will take arguements
 
-let equation = []
 
-// 2. Display current number on display
 
-// 3. Display number in calculation summary window in display
+
+// Get value using an event listener.
+
+var UI = {};
+
+UI.selectClick = function() {
+    [...document.querySelectorAll('.number-input-js')].forEach(function(node) {
+        node.addEventListener('click', function() {
+            alert('I have been clicked baby!')
+            let selectedNumber = node.value;
+            console.log(selectedNumber)
+        })
+    })    
+}
+UI.selectClick();
+
+// UI.display = function() {
+    
+// }
+
+// Display current number on display
+
+// Display number in calculation summary window in display
 
 
 // Flatten out array to do calculation (use a FOR loop to cocatenate everything?)
